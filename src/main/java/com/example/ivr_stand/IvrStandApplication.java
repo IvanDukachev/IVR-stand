e2db9627313c1ab2a563d0c1d3bf76a4ba804ca0
IvanDukachev <ivandukachev2002@gmail.com>
2024-10-12 14:28:09 +0500

    CA-26 build: install and configure project dependencies for local setup

---
+++ src/main/java/com/example/ivr_stand/IvrStandApplication.java
@@ -0,0 +1,13 @@
+package com.example.ivr_stand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvrStandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvrStandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ivr_stand/IvrStandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ivr_stand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvrStandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
