89226a7ee6357d5bfb1177303d78c2de534718ff
pv_hum <cool_syskov@mail.ru>
2024-10-30 14:42:42 +0500

    CA-42 feat: implement endpoint to retrieve detailed information for selected IVR scenario

--- src/main/java/com/example/ivr_stand/ItemController.java
+++ src/main/java/com/example/ivr_stand/ItemController.java
@@ -3,18 +3,33 @@ package com.example.ivr_stand;
 import com.example.ivr_stand.model.Item;
 import com.example.ivr_stand.repo.ItemRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ItemController {
     @Autowired
-    ItemRepo item;
+    private ItemRepo itemRepo;
 
     @GetMapping("/getItems")
-    public List<Item> getItems(){
-        return  item.findAll();
+    public List<Item> getItems() {
+        return itemRepo.findAll();
+    }
+
+
+    @GetMapping("/getItem/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable Integer id) {
+        Optional<Item> item = itemRepo.findById(id);
+
+        if (item.isPresent()) {
+            return ResponseEntity.ok(item.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
